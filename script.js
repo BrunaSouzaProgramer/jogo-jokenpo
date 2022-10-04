3546cbe6beaf4fe0f4862e395fb142bfe24632b7
BrunaSouzaProgramer <brunasouza.programer@gmail.com>
2022-10-03 21:16:14 -0300

    adicione placar dos jogadores

--- script.js
+++ script.js
@@ -1,57 +1,59 @@
 let maoJogador = document.querySelectorAll(".mao-jogador")
-let maoComputador = document.querySelectorAll(".mao-computador")
 let escolhaJogador = document.querySelector(".escolha-jogador")
 let escolhaComputador = document.querySelector(".escolha-computador")
-let respostaVencedor = document.querySelector(".resposta-vencedor")
-let placarJogador = document.querySelector("#placar-jogador")
-let placarComputador = document.querySelector("#placar-computador")
+let maoComputador= document.querySelectorAll(".mao-computador")
+let respostaVencedor= document.querySelector(".resposta-vencedor")
+let placarJogador=document.querySelector("#placar-jogador")
+let placarComputador=document.querySelector("#placar-computador")
 
-let vitoriasJogador = 0
-let vitoriasComputador = 0
+let vitoriasJogador=0
+let vitoriasComputador=0
 
-maoJogador.forEach((mao, indexJogador) => {
+maoJogador.forEach((mao, indicejogador) => {
    mao.addEventListener("click", (e) => {
-      escolhaJogador.src = mao.src
-      let numeroRandom = Math.floor(Math.random() * 3)
-      escolhaComputador.src = maoComputador[numeroRandom].src
-      maoComputador.forEach((mao, indexComputador) => {
-         console.log(mao)
-         if(indexComputador == numeroRandom){
-            mao.style.backgroundColor = "red"
-         } else {
-            mao.style.backgroundColor = "beige"
-         }
+      escolhaJogador.src=mao.src
+      let numeroAleatorio=Math.floor(Math.random()*3)
+      escolhaComputador.src=maoComputador[numeroAleatorio].src
+      maoComputador.forEach((mao, indice) =>{
+            if(indice==numeroAleatorio){
+               mao.style.backgroundColor = "pink"
+            }else{
+               mao.style.backgroundColor = "beige"
+            }
       })
+      respostaVencedor.innerText=quemVenceu(indicejogador, numeroAleatorio)
+      placarJogador.innerText=String(vitoriasJogador)
+      placarComputador.innerText=String(vitoriasComputador)
 
-      respostaVencedor.innerText = quemVenceu(indexJogador, numeroRandom)
-      placarJogador.innerText = String(vitoriasJogador)
-      placarComputador.innerText = String(vitoriasComputador)
    })
 })
 
-function quemVenceu(numeroJogador, numeroComputador){
-   if(numeroJogador == numeroComputador){
+
+
+function quemVenceu(numerojogador, numerocomputador){
+   if(numerojogador==numerocomputador){
       return "empate"
-   } else if(numeroJogador == 0 && numeroComputador == 1){
-      vitoriasComputador = vitoriasComputador + 1
-      return "Computador venceu"
-   } else if(numeroJogador == 0 && numeroComputador == 2){
-      vitoriasJogador = vitoriasJogador + 1
-      return "Jogador venceu"
-   } else if(numeroJogador == 1 && numeroComputador == 0){
-      vitoriasJogador = vitoriasJogador + 1
-      return "Jogador venceu"
-   } else if(numeroJogador == 1 && numeroComputador == 2){
-      vitoriasComputador = vitoriasComputador + 1
-      return "Computador venceu"
-   } else if(numeroJogador == 2 && numeroComputador == 0){
-      vitoriasComputador = vitoriasComputador + 1
-      return "Computador venceu"
-   } else if(numeroJogador == 2 && numeroComputador == 1){
-      vitoriasJogador = vitoriasJogador + 1
-      return "Jogador venceu"
-   } else {
-      return "Algo deu errado"
+
+   }else if(numerojogador==0 && numerocomputador==1){
+      vitoriasComputador=vitoriasComputador+1
+      return "computador venceu"   
+   }else if(numerojogador==0 && numerocomputador==2){
+      vitoriasJogador=vitoriasJogador+1
+      return "jogador venceu"
+   }else if(numerojogador==1 && numerocomputador==0){
+      vitoriasJogador=vitoriasJogador+1
+      return "jogador venceu"
+   }else if(numerojogador==1 && numerocomputador==2){
+      vitoriasComputador+=1
+      return "computador venceu"
+   }else if(numerojogador==2 && numerocomputador==0){
+      vitoriasComputador+=1
+      return "computador venceu"
+   }else if(numerojogador==2 && numerocomputador==1){
+      vitoriasJogador+=1
+      return "jogador venceu"
    }
-}
+
+ }
+
 
